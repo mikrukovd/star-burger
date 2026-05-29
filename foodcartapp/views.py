8ddbe77a4be9d49ebf74b21ee152f73955967c71
mikrukovd <z123456689z@gmail.com>
2026-05-29 16:05:16 +0300

    Add transaction

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.http import JsonResponse
 from django.templatetags.static import static
 
@@ -91,6 +92,7 @@ def product_list_api(request):
 
 
 @api_view(["POST"])
+@transaction.atomic
 def register_order(request):
 
     serializer = OrderSerializer(data=request.data)
