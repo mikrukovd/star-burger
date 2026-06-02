4e486a6c6f4e8fc34942ff319c5a0d1dbf2bb9d2
mikrukovd <z123456689z@gmail.com>
2026-06-02 10:36:09 +0300

    Add redirect from admin panel to manager/orders/ page

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.shortcuts import reverse
+from django.shortcuts import redirect, reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import (
     Order,
@@ -151,3 +152,18 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         OrderItemInline,
     ]
+
+    def response_change(self, request, obj):
+        next_url = request.GET.get("next")
+
+        if (
+            next_url
+            and "_save" in request.POST  # _save кнопка сохранить в админке
+            and url_has_allowed_host_and_scheme(
+                next_url,
+                allowed_hosts={request.get_host()},
+            )
+        ):
+            return redirect(next_url)
+
+        return super().response_change(request, obj)
