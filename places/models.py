00372a16238555676d3da39d99651fcfdfc75849
mikrukovd <z123456689z@gmail.com>
2026-06-09 08:52:06 +0300

    Add verbose_name

--- places/models.py
+++ places/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Place(models.Model):
-    address = models.CharField(max_length=200, unique=True)
-    lon = models.FloatField(null=True)
-    lat = models.FloatField(null=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    address = models.CharField("адрес", max_length=200, unique=True)
+    lon = models.FloatField("долгота", null=True)
+    lat = models.FloatField("широта", null=True)
+    updated_at = models.DateTimeField("дата последнего обновления", auto_now=True)
 
     class Meta:
         verbose_name = "место"
